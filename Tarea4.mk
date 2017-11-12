e270b1d12744ce2b5e3d54396bfc47cd724eb880
Violeta Rodríguez <violetar48@gmail.com>
2017-11-11 21:55:44 -0500

    Creando MakeFile

--- Tarea4.mk
+++ Tarea4.mk
@@ -1,1 +1,5 @@
 #Makefile de la tarea 4. 
+all : Plots.py
+
+Plots.py: resultados1DFijo.dat resultados1Dperturbado.dat resultados2D0.dat resultados2D8.dat resultados2D4.dat resultados2D2.dat
+	python Plots.py
